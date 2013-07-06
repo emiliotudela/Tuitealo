e978f1c57b4ee88b4381476e490f40c0caa7e0c8
emiliotudela <infoemiliotudela@gmail.com>
2013-07-06 10:45:58 -0700

    config.php
    Configutation

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @file
+ * A single location to store configuration.
+ */
+
+define('CONSUMER_KEY', 'i9QJRW2dlRKlvV6ONnn8jg');
+define('CONSUMER_SECRET', 'b2Jw7h5TxBG7npDYCnuDhaXR4BnMgIiS7VaxoDyhPgg');
+define('OAUTH_CALLBACK', 'http://tuitealo.mediaweblabs.com/callback.php');
